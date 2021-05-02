31d1724086187216edc902e91ff5ee2ec02bef4f
David Bastos <davtechh@gmail.com>
2021-05-02 09:48:16 -0300

    Adicionando components de bootstrap e font Open Sans

--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Global persons config
 import './assets/scss/main.scss'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
